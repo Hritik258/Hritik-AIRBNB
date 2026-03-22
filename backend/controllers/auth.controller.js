a16e03fce043438c4eae697e46b96ac1b7c444d0
Hritik258 <ranahritik25@gmail.com>
2026-03-22 17:58:12 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,8 +22,8 @@ export const signup = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENVIRONMENT === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -53,8 +53,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENVIRONMENT === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -70,8 +70,8 @@ export const logOut = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENVIRONMENT === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
     });
     return res.status(200).json({ message: "Logout successful" });
   } catch (error) {
