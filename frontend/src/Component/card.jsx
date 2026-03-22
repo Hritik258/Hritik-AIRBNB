e0fd9e4f36a97342589af11b2aab6c288658b1e9
Hritik258 <ranahritik25@gmail.com>
2026-03-22 19:04:43 +0530

    Update card.jsx

--- frontend/src/Component/card.jsx
+++ frontend/src/Component/card.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { listingDataContext } from '../Context/ListingContext'
+export const listingDataContext = createContext();
 
 const Card = () => {
   const navigate = useNavigate()
