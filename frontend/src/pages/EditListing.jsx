ff029896b1f8cfc9be3c3f5f6f914e71e1b82ee2
Hritik258 <ranahritik25@gmail.com>
2026-03-22 18:47:09 +0530

    Update EditListing.jsx

--- frontend/src/pages/EditListing.jsx
+++ frontend/src/pages/EditListing.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { listingDataContext } from '../Context/listingContext'
+import { listingDataContext } from '../Context/ListingContext'
 import { AuthDataContext } from '../Context/AuthContext'
 import { FaArrowLeft } from 'react-icons/fa'
 import axios from 'axios'
